f6bda900a10b83c67c512e140697a7adaccf0e2e
Imash-Dilhara <imashdilhara1225@gmail.com>
2026-09-04 23:26:16 +0530

    almost done the home page

--- index.php
+++ index.php
@@ -13,20 +13,26 @@
     </header>
 
     <div class="contentBox">
+        <h1 class="title">Explore Worship Hymns</h1>
+        <p class="searchPara">Discover lyrics and curate songs for your upcoming worship session</p>
         <div class="searchBox">
             <input type="text" class="searchBar" placeholder="Search for a hymn...">
             <img src="assets/search-icon.png" class="searchIcon" alt="Search Icon">
         </div>
         <div class="sideBox">
             <div class="left">
-                <h1 class="boxName">View Hymns</h1>
-                <p>"Discover all available hymns in our database. Click here to browse through the entire collection, find your favorite worship songs, and view their full details."</p>
-                <h3>Click Here</h3>
+                <div>
+                    <h1 class="boxName">View Hymns</h1>
+                    <p>"Discover all available hymns in our database. Click here to browse through the entire collection, find your favorite worship songs, and view their full details."</p>
+                    <h3>Click Here</h3>
+                </div>
             </div>
             <div class="right">
-                <h1 class="boxName">Create Hymn Slot</h1>
-                <p>"Plan and organize your worship service with ease. Select up to 10 hymns from the library, customize their order, and launch them into a presentation-ready slot."</p>
-                <h3>Click Here</h3>
+                <div>
+                    <h1 class="boxName">Create Hymn Slot</h1>
+                    <p>"Plan and organize your worship service with ease. Select up to 10 hymns from the library, customize their order, and launch them into a presentation-ready slot."</p>
+                    <h3>Click Here</h3>
+                </div>
             </div>
         </div>
     </div>
